0e62425d33c4566f9495ae526c6ec6cc17262ae8
Pushpendra Singh <99976461+govind124@users.noreply.github.com>
2022-09-21 18:47:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+const http = require("http");
+const server = http.createServer(function (req, res) {
+    res.write("Server Creation \n");
+    res.write("<h1>Server Creation</h1> \n");
+    res.write(JSON.stringify({ Name: "Pushpendra Singh" }))
+    res.end();
+})
+server.listen(5000)
